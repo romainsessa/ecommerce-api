ec536723b08a0931dd432f859f44d7e55bd76af1
romainsessa <romainsessa@gmail.com>
2022-02-14 22:44:55 +0100

    fix cross origin issue

--- src/main/java/com/esiea/ecommerceapi/controller/CategoryController.java
+++ src/main/java/com/esiea/ecommerceapi/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.esiea.ecommerceapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.esiea.ecommerceapi.service.CategoryService;
 import com.esiea.ecommerceapi.service.ProductService;
 import com.esiea.ecommerceapi.transformer.category.CategoryFull;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("api/private")
 public class CategoryController {
--- src/main/java/com/esiea/ecommerceapi/controller/ProductController.java
+++ src/main/java/com/esiea/ecommerceapi/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -19,6 +20,7 @@ import com.esiea.ecommerceapi.service.NotFoundException;
 import com.esiea.ecommerceapi.service.ProductService;
 import com.esiea.ecommerceapi.transformer.product.ProductFull;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("api/private")
 public class ProductController {
